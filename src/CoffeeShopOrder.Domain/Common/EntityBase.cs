c919a7ec18dc855bf394ae22a91ac4c2f57b5b09
Romario de M Damasceno <romario.auth@gmail.com>
2024-05-16 00:49:29 -0300

    Edit entity base

--- src/CoffeeShopOrder.Domain/Common/EntityBase.cs
+++ src/CoffeeShopOrder.Domain/Common/EntityBase.cs
@@ -3,11 +3,23 @@
     public abstract class EntityBase
     {
         public Guid Id { get; protected set; }
-        public Guid CreatedBy { get; set; }
+        public Guid? CreatedBy { get; set; }
         public DateTime CreatedAt { get; set; }
-        public Guid UpdatedBy { get; set; }
-        public DateTime? UpdatedAt { get; set; }
+        public Guid? UpdatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
         public bool IsValid() => Validate().Count == 0;
         public abstract List<string> Validate();
+        public void CreateNew(Guid? createdBy)
+        {
+            Id = Guid.NewGuid();
+            CreatedBy = createdBy;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void Updated(Guid? updatedBy)
+        {
+            UpdatedBy = updatedBy;
+            UpdatedAt = DateTime.UtcNow;
+        }
     }
 }
