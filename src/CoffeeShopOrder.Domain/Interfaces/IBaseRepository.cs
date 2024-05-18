b36db0c2bdbf78e32b75f258ed0b7778c7e2ed05
Romario de M Damasceno <romario.auth@gmail.com>
2024-05-18 20:12:08 -0300

    Change return T to void in IBaseRepository

--- src/CoffeeShopOrder.Domain/Interfaces/IBaseRepository.cs
+++ src/CoffeeShopOrder.Domain/Interfaces/IBaseRepository.cs
@@ -4,8 +4,8 @@ namespace CoffeeShopOrder.Domain.Interfaces
 {
     public interface IBaseRepository<T> where T : EntityBase
     {
-        T Create(T entity);
-        T Update(T entity);
+        void Create(T entity);
+        void Update(T entity);
         void Delete(T entity);
         Task<T> Get(Guid T, CancellationToken cancellation);
         Task<List<T>> GetAll(CancellationToken cancellation);
