678d40a591d0d166addad7fe1a7b4a7c6e201fe2
Romario de M Damasceno <romario.auth@gmail.com>
2024-05-18 20:14:00 -0300

    Set IUserInterface as public

--- src/CoffeeShopOrder.Domain/Interfaces/IUserRepository.cs
+++ src/CoffeeShopOrder.Domain/Interfaces/IUserRepository.cs
@@ -2,7 +2,7 @@
 
 namespace CoffeeShopOrder.Domain.Interfaces
 {
-    interface IUserRepository : IBaseRepository<User>
+    public interface IUserRepository : IBaseRepository<User>
     {
     }
 }
