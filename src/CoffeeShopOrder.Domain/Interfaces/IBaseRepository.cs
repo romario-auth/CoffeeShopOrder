44d1c7e4d93b4713f92c72af9b9c31a96b961c9d
Romario de M Damasceno <romario.auth@gmail.com>
2024-05-18 19:52:40 -0300

    Create interface from Domain

---
+++ src/CoffeeShopOrder.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using CoffeeShopOrder.Domain.Common;
+
+namespace CoffeeShopOrder.Domain.Interfaces
+{
+    public interface IBaseRepository<T> where T : EntityBase
+    {
+        T Create(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+        Task<T> Get(Guid T, CancellationToken cancellation);
+        Task<List<T>> GetAll(CancellationToken cancellation);
+    }
+}
---
+++ src/CoffeeShopOrder.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using CoffeeShopOrder.Domain.Entities;
+
+namespace CoffeeShopOrder.Domain.Interfaces
+{
+    interface IUserRepository : IBaseRepository<User>
+    {
+    }
+}
